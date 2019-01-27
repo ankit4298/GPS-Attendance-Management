2600184c71f93791b91a6d76ed561f1720fa5f5e
ankit4298 <ankit4298@gmail.com>
2019-01-27 14:03:39 +0530

    Login Validation

---
+++ src/java/serializable_classes/LoginInfo.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serializable_classes;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author ANKIT
+ */
+public class LoginInfo implements Serializable{
+    
+    private String uname;
+    private String pass;
+    private String loginstatus;
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getLoginstatus() {
+        return loginstatus;
+    }
+
+    public void setLoginstatus(String loginstatus) {
+        this.loginstatus = loginstatus;
+    }
+    
+    
+}
